b85cffac745f65f95b40382c6df9cc20e827a689
Oscar Ko <OscarKoEmail@gmail.com>
2023-03-08 10:38:33 -0500

    Initial commit.

---
+++ main.js
@@ -0,0 +1,14 @@
+/* CONSTANTS AND GLOBALS */
+const width = window.innerWidth *.8 ;
+const height = window.innerHeight *.8 ;
+
+// const margin = {
+//     right: 50,
+//     left: 50,
+//     top: 50,
+//     bottom: 50
+// }
+
+// /* LOAD DATA */
+
+console.log("Hello")
