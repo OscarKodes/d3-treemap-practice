373593b6db9d31373ea926a228624ae512f76e5f
Oscar Ko <OscarKoEmail@gmail.com>
2023-03-08 11:25:18 -0500

    Create treemap and add tippy tool tips

--- main.js
+++ main.js
@@ -5,116 +5,81 @@ const width = window.innerWidth * 0.9,
 height = window.innerHeight * 0.7,
 margin = { top: 20, bottom: 50, left: 60, right: 40 };
 
-let svg;
-let tooltip;
-
-/**
-* APPLICATION STATE
-* */
-let state = {
-data: null,
-hover: null,
-mousePosition: null,
-};
 
 /**
 * LOAD DATA
 * */
 d3.json("/flare.json", d3.autotype).then(data => {
- state.data = data;
- init();
+    console.log(data);
+
+    const svg = d3.select("#container")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .style("background-color", "lavender");
+   
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+   
+    // make hierarchy
+    const root = d3
+      .hierarchy(data) // children accessor
+      .sum(d => d.value) // sets the 'value' of each level
+      .sort((a, b) => b.value - a.value);
+   
+    // make treemap layout generator
+    const tree = d3
+      .treemap()
+      .size([width, height])
+      .padding(1)
+      .round(true);
+   
+    // call our generator on our root hierarchy node
+    tree(root); // creates our coordinates and dimensions based on the hierarchy and tiling algorithm
+   
+    // create g for each leaf
+    const leaf = svg
+      .selectAll("g")
+      .data(root.leaves())
+      .join("g")
+      .attr("transform", d => `translate(${d.x0},${d.y0})`);
+   
+    leaf
+      .append("rect")
+      .attr("fill", d => {
+        const level1Ancestor = d.ancestors().find(d => d.depth === 1);
+        return colorScale(level1Ancestor.data.name);
+      })
+      .attr("width", d => d.x1 - d.x0)
+      .attr("height", d => d.y1 - d.y0);
+
+
+
+    // ============================================
+
+    // // set default arrows on tool tips
+    // tippy.setDefaults({
+    //   "arrow": true
+    // })
+
+    // set the tooltip content
+    leaf
+      .attr("allowHTML", true)
+      .attr("data-tippy-content", d => {
+
+        let level1Ancestor = d.ancestors().find(d => d.depth === 1)
+        let level2Ancestor = d.ancestors().find(d => d.depth === 2)
+        let level3Ancestor = d.ancestors().find(d => d.depth === 3)
+
+        let n1 = level1Ancestor ? level1Ancestor.data.name : "NA";
+        let n2 = level2Ancestor ? level2Ancestor.data.name : "NA";
+        let n3 = level3Ancestor ? level3Ancestor.data.name : "NA";
+
+        let txt = `${n1} > ${n2} > ${n3}`
+
+        return txt;
+      })
+
+
+    // call tippy on the dots
+    tippy(leaf.nodes());
 });
-/**
-* INITIALIZING FUNCTION
-* this will be run *one time* when the data finishes loading in
-* */
-function init() {
- const container = d3.select("#container").style("position", "relative");
-
- tooltip = container
-   .append("div")
-   .attr("class", "tooltip")
-   .attr("width", 100)
-   .attr("height", 100)
-   .style("position", "absolute");
-
- svg = container
-   .append("svg")
-   .attr("width", width)
-   .attr("height", height);
-
- const colorScale = d3.scaleOrdinal(d3.schemeSet3);
-
- // make hierarchy
- const root = d3
-   .hierarchy(state.data) // children accessor
-   .sum(d => d.value) // sets the 'value' of each level
-   .sort((a, b) => b.value - a.value);
-
- // make treemap layout generator
- const tree = d3
-   .treemap()
-   .size([width, height])
-   .padding(1)
-   .round(true);
-
- // call our generator on our root hierarchy node
- tree(root); // creates our coordinates and dimensions based on the hierarchy and tiling algorithm
-
- // create g for each leaf
- const leaf = svg
-   .selectAll("g")
-   .data(root.leaves())
-   .join("g")
-   .attr("transform", d => `translate(${d.x0},${d.y0})`);
-
- leaf
-   .append("rect")
-   .attr("fill", d => {
-     const level1Ancestor = d.ancestors().find(d => d.depth === 1);
-     return colorScale(level1Ancestor.data.name);
-   })
-   .attr("width", d => d.x1 - d.x0)
-   .attr("height", d => d.y1 - d.y0)
-   .on("mouseover", (m, d) => {
-     state.hover = {
-       translate: [
-         // center top left corner of the tooltip in center of tile
-         d.x0 + (d.x1 - d.x0) / 2,
-         d.y0 + (d.y1 - d.y0) / 2,
-       ],
-       name: d.data.name,
-       value: d.data.value,
-       title: `${d
-         .ancestors()
-         .reverse()
-         .map(d => d.data.name)
-         .join("/")}`,
-     };
-     draw();
-   });
-
- draw(); // calls the draw function
-}
-/**
-* DRAW FUNCTION
-* we call this every time there is an update to the data/state
-* */
-function draw() {
- if (state.hover) {
-   tooltip
-     .html(
-       `
-       <div>Name: ${state.hover.name}</div>
-       <div>Value: ${state.hover.value}</div>
-       <div>Hierarchy Path: ${state.hover.title}</div>
-     `
-     )
-     .transition()
-     .duration(500)
-     .style(
-       "transform",
-       `translate(${state.hover.translate[0]}px,${state.hover.translate[1]}px)`
-     );
- }
-}
